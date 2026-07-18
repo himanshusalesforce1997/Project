5dfec3371c63e7fd3f35959aad25e57493b1e993
Himanshu Soni <himanshusalesforce7197@gmail.com>
2026-07-19 00:30:58 +0530

    Add a project folder with two files

---
+++ Day1/First.js
@@ -0,0 +1,10 @@
+const addNumbers = (a, b) => a + b;
+const subtractNumbers = (a, b) => a - b;
+
+const num1 = 10;
+const num2 = 20;
+const sumResult = addNumbers(num1, num2);
+const subtractResult = subtractNumbers(num1, num2);
+
+console.log(`Sum of ${num1} and ${num2} is ${sumResult}`);
+console.log(`Difference of ${num1} and ${num2} is ${subtractResult}`);
---
+++ Day1/testalgebra.js
@@ -0,0 +1,14 @@
+const addNumbers = (a, b) => a + b;
+const subtractNumbers = (a, b) => a - b;
+
+// Addition test cases
+console.log(addNumbers(2, 3)); // 5
+console.log(addNumbers(10, 20)); // 30
+console.log(addNumbers(-1, 1)); // 0
+console.log(addNumbers(0, 0)); // 0
+
+// Subtraction test cases
+console.log(subtractNumbers(5, 2)); // 3
+console.log(subtractNumbers(20, 10)); // 10
+console.log(subtractNumbers(-1, 1)); // -2
+console.log(subtractNumbers(0, 0)); // 0
